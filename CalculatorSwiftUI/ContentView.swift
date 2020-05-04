1a58ec6640a9e40a3963420f0d55c720e646352c
Connor Lagana <connor.lagana@gmail.com>
2020-05-04 18:27:17 -0400

    enums

--- CalculatorSwiftUI/ContentView.swift
+++ CalculatorSwiftUI/ContentView.swift
@@ -8,9 +8,54 @@
 
 import SwiftUI
 
+enum  CalculatorButton: String {
+    case zero, one, two, three, four, five, six, seven, eight, nine
+    case equals, plus, minus, multiply, divide
+    case ac, plusminus, percent
+    
+    var title: String {
+        switch self {
+        case .zero: return "0"
+        case .one: return "1"
+        case .two: return "2"
+        case .three: return "3"
+        case .four: return "4"
+        case .five: return "5"
+        case .six: return "6"
+        case .seven: return "7"
+        case .eight: return "8"
+        case .nine: return "9"
+        case .plus: return "+"
+        case .minus: return "-"
+        case .equals: return "="
+        case .divide: return "÷"
+        case .multiply: return "x"
+        case .plusminus: return "±"
+        case .ac: return "AC"
+        default:
+            return "Con"
+        }
+    }
+    
+    var backgroundColor: Color {
+        switch self {
+        case .zero, .one, .two, .three, .four, .five, .six, .seven, .eight, .nine:
+            return Color(.darkGray)
+        case .ac:
+            return Color(.lightGray)
+        default:
+            return .orange
+        }
+    }
+}
+
 struct ContentView: View {
     
-    let buttons = [["AC", "±", "%", "÷"], ["7", "8", "9", "x"], ["4", "5", "6", "-"], ["1", "2", "3", "+"], ["0", ".", ".", "="]]
+    let buttons: [[CalculatorButton]] = [
+        [.seven, .eight, .nine, .multiply],
+        [.four, .five, .six, .minus],
+        [.one, .two, .three, .plus]
+    ]
     
     var body: some View {
         ZStack (alignment: .bottom) {
@@ -25,10 +70,10 @@ struct ContentView: View {
                 ForEach(buttons, id: \.self) { row in
                     HStack {
                         ForEach(row, id: \.self) { button in
-                            Text(button)
+                            Text(button.title)
                                 .font(.system(size: 32))
                                 .frame(width: self.buttonFrame(), height: self.buttonFrame())
-                                .background(Color.gray)
+                                .background(button.backgroundColor)
                                 .clipShape(Circle())
                                 
                                 .foregroundColor(.white)
