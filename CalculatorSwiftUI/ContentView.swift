16dd8412e8b7dbbb6b0e996e6dc248592baed3d2
Connor Lagana <connor.lagana@gmail.com>
2020-05-04 17:57:28 -0400

    black background

--- CalculatorSwiftUI/ContentView.swift
+++ CalculatorSwiftUI/ContentView.swift
@@ -13,24 +13,28 @@ struct ContentView: View {
     let buttons = [["AC", "±", "%", "÷"], ["7", "8", "9", "x"], ["4", "5", "6", "-"], ["1", "2", "3", "+"]]
     
     var body: some View {
-        VStack {
-            ForEach(buttons, id: \.self) { row in
-                HStack {
-                    ForEach(row, id: \.self) { button in
-                        Text(button)
-                            .font(.system(size: 32))
-                            .frame(width: 80, height: 80)
-                            .background(Color.gray)
-                            .clipShape(Circle())
-                            .padding(2)
-                            .foregroundColor(.white)
-                        
+        ZStack {
+            Color.black.edgesIgnoringSafeArea(.all)
+            VStack {
+                ForEach(buttons, id: \.self) { row in
+                    HStack {
+                        ForEach(row, id: \.self) { button in
+                            Text(button)
+                                .font(.system(size: 32))
+                                .frame(width: 80, height: 80)
+                                .background(Color.gray)
+                                .clipShape(Circle())
+                                .padding(2)
+                                .foregroundColor(.white)
+                            
+                        }
                     }
+                    
                 }
                 
             }
-            
         }
+        
     }
 }
 
