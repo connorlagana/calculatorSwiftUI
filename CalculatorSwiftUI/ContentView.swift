7b9657dfd71456341d213d92487c7aec6e89be6f
Connor Lagana <connor.lagana@gmail.com>
2020-05-04 17:42:05 -0400

    Initial Commit

---
+++ CalculatorSwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CalculatorSwiftUI
+//
+//  Created by Connor Lagana on 5/4/20.
+//  Copyright © 2020 Connor Lagana. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
